30634cb77e7c146c6fc4a5ef6d4d679a0809bc62
anurag <anchouhan@bestpeers.com>
2026-03-29 14:39:29 +0530

    added navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,14 @@ import Login from "./pages/Login";
 import ProtectedRoute from "./components/protectedRote";
 import PublicRoute from "./components/publicRoute";
 import SelectRole from "./pages/SelectRole";
+import Navbar from "./components/Navbar";
 
 const App = () => {
 
   return (
     <>
       <BrowserRouter>
-       
+       <Navbar/>
         <Routes>
           <Route element={<PublicRoute />}>
             <Route path="/login" element={<Login />} />
